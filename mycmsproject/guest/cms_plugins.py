a963770b5cad4c54f67d21aafdd2b39981d615a4
Canbey Bilgili <cbilgili@gmail.com>
2013-06-25 12:41:32 +0300

    Anaysayfa ve kategori

--- mycmsproject/guest/cms_plugins.py
+++ mycmsproject/guest/cms_plugins.py
@@ -2,6 +2,7 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from cms.models.pluginmodel import CMSPlugin
 from django.utils.translation import ugettext_lazy as _
+from products.models import *
 
 from models import Hello
 
@@ -11,7 +12,10 @@ class HelloPlugin(CMSPluginBase):
     render_template = "hello_plugin.html"
 
     def render(self, context, instance, placeholder):
+
+        product = Product.objects.all().order_by('?')[:1].get()
         context['instance'] = instance
+        context['product'] = product
         return context
 
 plugin_pool.register_plugin(HelloPlugin)
